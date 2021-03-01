2831095edf5dd1326e67c3484a34830cab4ac78b
Huzaifa Shakir <huzaifa.ms@gmail.com>
2021-03-01 15:34:50 +1100

    drawers designs and tweaks

---
+++ src/app/components/headshot/headshot.component.ts
@@ -0,0 +1,51 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
+
+@Component({
+  selector: 'app-headshot',
+  templateUrl: './headshot.component.html',
+  styleUrls: ['./headshot.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeadshotComponent implements OnInit {
+  @Input() imageThumbnailUrl: string;
+  @Input() imageUrl: string;
+  @Input() initials: string;
+  @Input() quickViewEnabled: boolean;
+  @Input() editEnabled = false;
+  @Input() actionUrl: string;
+
+  @Output() click = new EventEmitter();
+  @Output() delete = new EventEmitter();
+  @Output() imageUploaded = new EventEmitter();
+
+  imageNotFound: boolean;
+  isFullViewVisible: boolean;
+  dragCounter: number;
+
+  ngOnInit(): void {
+    console.log('aaaaaa', this.imageThumbnailUrl, this.initials);
+  }
+
+  onImageLoadError() {
+    this.imageNotFound = true;
+  }
+
+  deleteImage() {
+    this.isFullViewVisible = false;
+    this.delete.emit();
+  }
+
+  fileChanged(uploadState: NzUploadChangeParam) {
+    if (uploadState.file.status === 'done') {
+      this.imageUploaded.emit(uploadState.file.response);
+    }
+  }
+}
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgZorroAntdModule } from 'src/app/nz-zorro.module';
+import { HeadshotComponent } from '../../components/headshot/headshot.component';
 import { SubmissionCandidateDrawerComponent } from '../../components/submission-candidate-drawer/submission-candidate-drawer.component';
 import { SubmissionDrawerComponent } from '../../components/submission-drawer/submission-drawer.component';
 
@@ -15,6 +16,7 @@ import { WelcomeComponent } from './welcome.component';
     WelcomeComponent,
     SubmissionDrawerComponent,
     SubmissionCandidateDrawerComponent,
+    HeadshotComponent,
   ],
   exports: [WelcomeComponent],
 })

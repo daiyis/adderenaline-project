2b7cc4c43f5f280ef0a4fd720561e547718cf1d9
Leo Dai <leo@jobadder.com>
2021-03-05 11:50:43 +1100

    add search function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { CandidateService } from './services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   isCollapsed = false;
+  searchChanged$ = new BehaviorSubject<string>('');
+  protected destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
+  constructor(private candidateService: CandidateService) {}
+
+  ngOnInit() {
+    this.initSearch();
+  }
+
+  private initSearch() {
+    this.searchChanged$
+      .pipe(takeUntil(this.destroyed$), debounceTime(250), distinctUntilChanged())
+      .subscribe((query) => {
+        this.candidateService.sendQueryMessage(query.trim());
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
 }
